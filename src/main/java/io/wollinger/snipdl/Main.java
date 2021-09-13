e6bc2cc1595df370a29c2ccf7ac7452728304551
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-09-13 15:12:20 +0200

    Initial commit

---
+++ src/main/java/io/wollinger/snipdl/Main.java
@@ -0,0 +1,9 @@
+package io.wollinger.snipdl;
+
+public class Main {
+    private static final String VERSION = "0.0.1";
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}

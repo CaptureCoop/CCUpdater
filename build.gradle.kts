c6d325159907d35f9f9bdf1d8d6a9320fd4c1934
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-09-22 11:50:05 +0200

    Add Kotlin to build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "org.capturecoop"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.wollinger:Zipper:v0.0.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes["Main-Class"] = "org.capturecoop.ccupdater.MainKt" }
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        sourceSets.main.get().output
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "CCUpdater"
+sourceControl {
+    gitRepository(uri("https://github.com/SvenWollinger/Zipper.git")) {
+        producesModule("io.wollinger:Zipper")
+    }
+}
